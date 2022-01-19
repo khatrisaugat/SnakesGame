685d1b82a41b1d4857b0ac7579acd6f9b2834f31
khatrisaugat <khatri.saugat55@gmail.com>
2022-01-19 20:35:39 +0545

    snake created

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ window.addEventListener('load',()=>{
     //state for keeping all the data
     let st={
         rows:15,
-        column:17
+        column:17,
+        SnakeBodyDetails:[],
+        initialSnakeLength:3
     };
     
     //View Selection
@@ -17,7 +19,9 @@ window.addEventListener('load',()=>{
             }
             return box;
         },
-        GameMap:document.createElement('div')
+        GameMap:document.createElement('div'),
+        SnakePart:()=>document.createElement('div'),
+        getMapStep:(step)=> document.querySelector('.step'+step)
     }
 
     //controller
@@ -30,7 +34,20 @@ window.addEventListener('load',()=>{
     mapView.map((el)=>{
         ViewData.GameMap.appendChild(el);
     });
-    
+
+    //Creating a Snake
+    const createSnake=()=>{
+        for (let i = 1; i <=st.initialSnakeLength ; i++) {
+            st.SnakeBodyDetails.push(((Math.round(st.rows/2)-1)*st.column)+i)
+        }
+        st.SnakeBodyDetails.map((el)=>{
+            let bodyPart=ViewData.SnakePart();
+            bodyPart.classList.add('snake');
+            ViewData.getMapStep(el).appendChild(bodyPart);
+        })
+    };
+    createSnake();
+    console.log(st.SnakeBodyDetails);    
 
 
 
