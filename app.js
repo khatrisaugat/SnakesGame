bd80d9ac02eeb02e73df2ff3dec9486ea42d66f8
khatrisaugat <khatri.saugat55@gmail.com>
2022-01-20 10:33:04 +0545

    all major activity working now

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ window.addEventListener('load',()=>{
         SnakeBodyDetails:[],
         initialSnakeLength:3,
         applePos:0,
-        direction:'R'
+        direction:'R',
+        speed:180,
+        score:0
     };
     
     //View Selection
@@ -25,19 +27,28 @@ window.addEventListener('load',()=>{
         GameMap:document.createElement('div'),
         getMapStep:(step)=> document.querySelector('.step'+step),
         getEl:(el)=>document.querySelector(el),
-        getAllEl:(el)=>document.querySelectorAll(el)
+        getAllEl:(el)=>document.querySelectorAll(el),
+        score:document.querySelector('.score'),
+        playBtn:document.createElement("button")
     }
 
     //controller
+    ViewData.playBtn.innerText="Play";
+    ViewData.playBtn.classList.add('playBtn');
+    ViewData.root.appendChild(ViewData.playBtn);
+    ViewData.playBtn.addEventListener('click',()=>StartGame());
 
     //creating a map for the game
-    ViewData.root.appendChild(ViewData.GameMap);
-    ViewData.GameMap.classList.add('GameMap');
-    ViewData.GameMap.style.width=(st.column*2)+'rem';
-    const mapView=ViewData.snakesMap(st.rows,st.column);
-    mapView.map((el)=>{
-        ViewData.GameMap.appendChild(el);
-    });
+    const createMap=()=>{
+        ViewData.root.appendChild(ViewData.GameMap);
+        ViewData.GameMap.classList.add('GameMap');
+        ViewData.GameMap.style.width=(st.column*2)+'rem';
+        ViewData.GameMap.style.height=(st.rows*2)+'rem';
+        const mapView=ViewData.snakesMap(st.rows,st.column);
+        mapView.map((el)=>{
+            ViewData.GameMap.appendChild(el);
+        });
+    }
 
     //Creating a Snake
     const createSnake=()=>{
@@ -50,7 +61,6 @@ window.addEventListener('load',()=>{
             ViewData.getMapStep(el).appendChild(bodyPart);
         })
     };
-    createSnake();
     console.log(st.SnakeBodyDetails);
     
     //creating apple
@@ -58,7 +68,7 @@ window.addEventListener('load',()=>{
         let apple=ViewData.createDivElement();
         //checking if there is an apple on the map before creating
         if(ViewData.getEl('body').contains(ViewData.getEl('.apple')))
-            ViewData.getEl('.apple').parentNode.removeChild(ViewData.getEl('.apple'));
+            ViewData.getAllEl('.apple').forEach(el=>el.parentNode.removeChild(el));
         apple.classList.add('apple');
         //creating and putting apple on the map
         const pos=Math.round(Math.random()*(st.rows*st.column));
@@ -67,9 +77,8 @@ window.addEventListener('load',()=>{
         }else{
             st.applePos=pos;
         } 
-        ViewData.getMapStep(pos).appendChild(apple);
+        ViewData.getMapStep(st.applePos).appendChild(apple);
     }
-    createApple();
 
     //append snake to new map positions according to change in head position
     const appendSnake=(head)=>{
@@ -81,38 +90,47 @@ window.addEventListener('load',()=>{
         ViewData.getAllEl('.snake').forEach((el,i)=>{
             ViewData.getMapStep(st.SnakeBodyDetails[i]).appendChild(el); 
         })
-        console.log(st.SnakeBodyDetails);
     }
 
     //Move Snake
     const moveSnake=()=>setInterval(()=>{
+        appleEaten();
+        bodyTouchCheck();
         EdgeCheck();
         if(st.direction==='R'){
             appendSnake(st.SnakeBodyDetails[0]+1);
-
+            
         }else if(st.direction==='L'){
-            appendSnake(st.SnakeBodyDetails[0]-1);
+            appendSnake(st.SnakeBodyDetails[0]-1);           
 
         }else if(st.direction==='U'){
-            appendSnake(st.SnakeBodyDetails[0]-17);
+            appendSnake(st.SnakeBodyDetails[0]-17);          
 
         }else if(st.direction==='D'){
             appendSnake(st.SnakeBodyDetails[0]+17);
 
         }
-    },250);
-    moveSnake();
+    },st.speed);
 
     //Edge touch check
     const EdgeCheck=()=>{
-        if(st.SnakeBodyDetails[0]<17){
-            alert('Game Over');
-        }else if(st.SnakeBodyDetails[0]%st.column===0){
-            alert('Game Over');
-        }else if((st.SnakeBodyDetails[0]-1)%st.column===0){
-            alert('Game Over');
-        }else if(st.SnakeBodyDetails[0]>st.column*st.rows){
-            alert('Game Over');
+        // console.log(st.SnakeBodyDetails);
+        if(st.SnakeBodyDetails[0]<1){
+            GameOver();
+        }else if(st.direction==='R' && (st.SnakeBodyDetails[0])%st.column===0){
+            GameOver();
+        }else if(st.direction==='L' && (st.SnakeBodyDetails[0]-1)%st.column===0){
+            GameOver();
+        }else if((st.SnakeBodyDetails[0])>st.column*st.rows){
+            GameOver();
+        }
+    }
+
+    //Body touch check
+    const bodyTouchCheck=()=>{
+        const tempBodyDetails=st.SnakeBodyDetails.map((el,i)=>i!==0&&el);
+        if(tempBodyDetails.includes(st.SnakeBodyDetails[0])){
+            GameOver();
         }
     }
 
@@ -130,6 +148,34 @@ window.addEventListener('load',()=>{
         }
     })
 
+    //check to see if apple eaten
+    const appleEaten=()=>{
+        if(st.SnakeBodyDetails[0]===st.applePos){
+            st.SnakeBodyDetails.push(st.SnakeBodyDetails.at(-1));
+            st.score++;
+            ViewData.score.innerHTML="Score: "+st.score;
+            let bodyPart=ViewData.createDivElement();
+            bodyPart.classList.add('snake');
+            ViewData.getMapStep(st.SnakeBodyDetails.at(-1)).appendChild(bodyPart);
+            console.log(st.SnakeBodyDetails);
+            console.log(st.score);
+            createApple();
+        }
+    }
+
+    const GameOver=()=>{
+        alert("Game Over!!\nYour score is "+st.score);
+        window.location.reload();
+
+    }
+    const StartGame=()=>{
+        ViewData.playBtn.parentNode.removeChild(ViewData.playBtn);
+        createMap();
+        createSnake();
+        createApple();
+        moveSnake();
+    }
+    // StartGame();
 
 
 
